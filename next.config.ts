d598325e671a742d5646225f77cc546c64d7eecf
cesarNietupski <44011116+cesarNietupski@users.noreply.github.com>
2026-09-08 22:43:55 -0300

    Ajusta basePath para o repositório CSR

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isGitHubPages ? "/meusite" : "",
+  basePath: isGitHubPages ? "/CSR" : "",
 };
 
 export default nextConfig;
